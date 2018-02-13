28393ade29efcb3c5ff0fdb74f731b521cf8d921
Chris Johnson <crjjrc@gmail.com>
2018-02-13 14:52:46 -0600

    Add Peggle's Arduino controller.

--- peggle/arduino/src/main.ino
+++ peggle/arduino/src/main.ino
@@ -1,17 +1,38 @@
 #include "Arduino.h"
 #define THRESHOLD 5
 
-int old_reading = 0;
+/* #define write println */
+
+int old_potentiometer;
+int old_button = LOW;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(7, INPUT);
+
+  emitPotentiometer(analogRead(A5));
+}
+
+void emitPotentiometer(int potentiometer) {
+  int byte = map(potentiometer, 0, 1023, 0, 255);
+  Serial.write(0);
+  Serial.write(byte);
+  old_potentiometer = potentiometer;
 }
 
 void loop() {
-  int reading = analogRead(A5);
-  if (abs(old_reading - reading) > THRESHOLD) {
-    int mapped = map(reading, 0, 1023, 0, 255);
-    Serial.write(mapped);
-    old_reading = reading;
+  int potentiometer = analogRead(A5);
+  if (abs(old_potentiometer - potentiometer) > THRESHOLD) {
+    emitPotentiometer(potentiometer);
+  }
+
+  int button = digitalRead(7);
+  if (button != old_button) {
+    if (button == LOW) {
+      Serial.write(1);
+      Serial.write(1);
+    }
+    old_button = button;
+    delay(10);
   }
 }
